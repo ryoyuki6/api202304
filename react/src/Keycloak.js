371f5a028dfbf13df5ad2398bd1bc89676972b42
R M <ryoyuki6@yahoo.co.jp>
2023-04-22 19:02:17 +0900

    qiita20230422

--- react/src/Keycloak.js
+++ react/src/Keycloak.js
@@ -1,7 +1,10 @@
 import Keycloak from "keycloak-js";
+import { ipAddress } from "./env";
+
+const url0 = "http://" + ipAddress + ":8080/auth";
 
 const keycloak = new Keycloak({
-  url: "http://35.77.214.212:8080/auth",
+  url: url0,
   realm: "test_realm",
   clientId: "react-auth",
 });
